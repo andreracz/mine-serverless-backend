4ee67ae96b5756f1ae0dea8239ce4aa5b9d8fc68
Andre Racz <andre.racz@avanade.com>
2021-11-06 07:48:55 -0300

    create

---
+++ servers/index.ts
@@ -0,0 +1,52 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { TableClient } from "@azure/data-tables"
+import { DefaultAzureCredential } from "@azure/identity"
+
+const account = "mineserverless";
+
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    
+    context.log('HTTP trigger function processed a request, ' + req.url);
+    const credential = new DefaultAzureCredential();
+    const tableClient = new TableClient(
+        `https://${account}.table.core.windows.net`,
+        'servers',
+        credential
+      );
+    const partitionKey = 'fixed';
+
+      if(req.method == 'GET') {
+        let entitiesIter = tableClient.listEntities();
+        let i = 1;
+        const resp = [];
+        for await (const entity of entitiesIter) {
+          resp.push({ serverName: entity.rowKey, size: entity.size });
+        }
+        context.res = {
+          // status: 200, /* Defaults to 200 */
+          body: resp
+        };
+  
+      } else if (req.method == 'POST') {
+        const body = req.body;
+        let entitiesIter = await tableClient.getEntity(partitionKey, body.serverName);
+        if (entitiesIter) {
+          context.res = {
+            status: 500,
+            body: 'Server alread exists'
+          };
+        } else {
+          await tableClient.createEntity({ partitionKey, rowKey: body.serverName, size: body.size });
+          context.res = {
+            // status: 200, /* Defaults to 200 */
+            boby: body
+          };
+        }
+      }
+
+
+
+};
+
+export default httpTrigger;
